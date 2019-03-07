da457c59ea3c0b8159222f528a60d61c4937e54e
Valentin Grudnik <wallicar99@gmail.com>
2019-03-07 22:23:10 +0100

    Shop/catering design, photos

--- resources/views/pages/catering.blade.php
+++ resources/views/pages/catering.blade.php
@@ -2,4 +2,222 @@
 
 @section('content')
  
-@endsection
+<!-- .page-content start -->
+<div class="page-content custom-img-background dark page-title page-title-1 mb-100" style="background-image: url('/storage/catering-bg.jpeg');" >
+        <div class="container">
+            <!-- .row start -->
+            <div class="row">
+                <!-- .col-md-12 start -->
+                <div class="col-md-12 centered">
+                    <div class="custom-heading style-1 triggerAnimation animated" data-animate='fadeInUp'>
+                        <h1><span></span></h1>
+                        <h1 style="color:black">CATERING ZA VSE PRILOŽNOSTI</h1>
+                    </div><!-- .custom-heading.style-1 end -->
+                </div><!-- .col-md-12 end -->
+            </div><!-- .row end -->
+        </div><!-- .container end -->
+    </div><!-- .page-content end -->
+
+    <!-- .page-content start -->
+    <div class="page-content">
+        <div class="container">
+            <!-- .row start -->
+            <div class="row">
+                <!-- .col-md-6 start -->
+                <div class="col-md-6 col-sm-6">
+                    <img src="/storage/hladne-jedi.jpeg" alt="Royal Plate - Restaurant & Catering HTML Template" />
+                </div><!-- .col-md-6 end -->
+                <!-- .col-md-6 start -->
+                <div class="col-md-6 col-sm-6 centered">
+                    <div class="custom-heading style-1">
+                        <h3><span>Priprava hladnih prigrizkov.</span></h3>
+                        <h3>HLADNI CATERING</h3>
+
+                        <!-- .divider.style-1 start -->
+                        <div class="divider style-1 center">
+                            <span class="hr-simple left"></span>
+                            <i class="fa fa-circle hr-icon"></i>
+                            <span class="hr-simple right"></span>
+                        </div>
+                    </div><!-- .custom-heading.style-1 end -->
+
+                    <p> V preteklosti smo bili predvsem specializirani za hladni catering: KANAPEJI, NAREZKI, PECIVO, SADJE, FINGER FOOD. </p>
+
+                </div><!-- .col-md-6 end -->
+            </div><!-- .row end -->
+        </div><!-- .container end -->
+    </div><!-- .page-content end -->
+
+    <!-- .page-content start -->
+    <div class="page-content">
+        <div class="container">
+            <!-- .row start -->
+            <div class="row">
+                <!-- .col-md-6 start -->
+                <div class="col-md-6 col-sm-6  centered">
+                    <div class="custom-heading style-1">
+                        <h3><span>Priprava toplih jedi.</span></h3>
+                        <h3>TOPLI CATERING</h3>
+
+                        <!-- .divider.style-1 start -->
+                        <div class="divider style-1 center">
+                            <span class="hr-simple left"></span>
+                            <i class="fa fa-circle hr-icon"></i>
+                            <span class="hr-simple right"></span>
+                        </div>
+                    </div><!-- .custom-heading.style-1 end -->
+
+                    <p>Sedaj pa smo z našimi novimi prostori ustvarili tudi pogoje za tople jedi: ENOLONČNICE, MALICE, KOSILA, TOPLO-HLADNI BIFE, ŽAR JEDI (že pripravljene jedi ali pečenje na samem mestu).</p>
+                </div><!-- .col-md-6 end -->
+                <!-- .col-md-6 start -->
+                <div class="col-md-6 col-sm-6">
+                    <img src="/storage/catering-tople-jedi.jpeg" alt="Royal Plate - Restaurant & Catering HTML Template" />
+                </div><!-- .col-md-6 end -->
+            </div><!-- .row end -->
+        </div><!-- .container end -->
+    </div><!-- .page-content end -->
+
+    <!-- .page-content start -->
+    <div class="page-content">
+        <div class="container">
+            <!-- .row start -->
+            <div class="row">
+                <!-- .col-md-6 start -->
+                <div class="col-md-6 col-sm-6">
+                    <img src="/storage/poroka.jpeg" alt="Royal Plate - Restaurant & Catering HTML Template" />
+                </div><!-- .col-md-6 end -->
+                <!-- .col-md-6 start -->
+                <div class="col-md-6 col-sm-6  centered">
+                    <div class="custom-heading style-1">
+                        <h3><span>Pomagamo vam</span></h3>
+                        <h3>Z ORGANIZACIJO DOGODKA</h3>
+
+                        <!-- .divider.style-1 start -->
+                        <div class="divider style-1 center">
+                            <span class="hr-simple left"></span>
+                            <i class="fa fa-circle hr-icon"></i>
+                            <span class="hr-simple right"></span>
+                        </div>
+                    </div><!-- .custom-heading.style-1 end -->
+
+                  
+
+                    <p>Če je pred vami organizacija dogodka (PRAZNOVANJE, POROKA, SREČANJE,…) se obrnite na NAS in pomagali vam bomo narediti, kar se da, CENOVNO UGODNO in KVALITETNO.  Seveda pa smo na voljo tudi samo za nasvet, ki pa pri nas nič ne stane. Stremimo k temu, da se skušamo čimbolj prilagoditi željam naročnika. Zato pri nas ne boste našli v naprej pripravljenih ponudb, ampak bomo za vsak dogodek, skupaj z vami, pripravili takšno izvedbo, ki je Vam blizu in všeč..</p>
+                   
+                </div><!-- .col-md-6 end -->
+            </div><!-- .row end -->
+        </div><!-- .container end -->
+    </div><!-- .page-content end -->
+
+    <!-- .page-content start -->
+    <div class="page-content">
+        <div class="container">
+            <!-- .row start -->
+            <div class="row">
+                <!-- .col-md-6 start -->
+                <div class="col-md-6 col-sm-6 centered">
+                    <div class="custom-heading style-1">
+                        <h3><span>Poleg priprave in dostave jedi tudi</span></h3>
+                        <h3>IZPOSOJA ŠOTORA TER INVENTARJA</h3>
+
+                        <!-- .divider.style-1 start -->
+                        <div class="divider style-1 center">
+                            <span class="hr-simple left"></span>
+                            <i class="fa fa-circle hr-icon"></i>
+                            <span class="hr-simple right"></span>
+                        </div>
+                    </div><!-- .custom-heading.style-1 end -->
+                    <ul class="article-meta">
+                        <li>ŠOTOR 10X5 metrov,</li>
+                        <li>CCA. 80 LJUDI,</li>
+                        <li>PRIPRAVA DEKORACIJ</li>
+                    </ul><!-- .article-meta end -->
+
+                    <p>Poleg priprave in dostave jedi, Vam nudimo tudi najem inventarja: prti, mize, barske mize, kozarci, grelne posode… V posojo pa dajemo tudi ŠOTOR 10X5 metrov, ki je primeren do cca. 80 ljudi
+
+                            Zelo nam je pomembna priprava prostora, zato poskrbimo tudi za dekoracijo v široki barvni lestvici.
+                            
+                            V primeru, da nimate primernega prostora pri Vas doma, Vam ga pomagamo najti in Vam glede na vaše želje in potrebe tudi priporočamo.
+                            
+                            </p>
+                   </div><!-- .col-md-6 end -->
+                <!-- .col-md-6 start -->
+                <div class="col-md-6 col-sm-6">
+                    <img src="/storage/dekoracija.jpeg" alt="Royal Plate - Restaurant & Catering HTML Template" />
+                </div><!-- .col-md-6 end -->
+            </div><!-- .row end -->
+        </div><!-- .container end -->
+    </div><!-- .page-content end -->
+
+    <div class="page-content">
+        <!-- .container start -->
+        <div class="container">
+            <h2>REFERENCE: </h2>
+            <!-- .row start -->
+            <div class="row">
+                <div class="col-md-4 col-sm-4">
+                    <div class="team-member">                                
+                        <figure>
+                            <img src="/storage/skaza.png" alt="Royal Plate - Restaurant & Catering HTML Template"/>
+
+                            <figcaption>
+                                <h4 class="member-name">Plastika SKAZA</h4>
+                          
+
+                                <!-- .divider start -->
+                                <div class="divider">
+                                    <span class="hr-simple"></span>
+                                </div>
+
+                                </figcaption>   
+                        </figure>
+                    </div><!-- .team-member end -->
+
+                </div><!-- .col-md-4 end -->
+
+                <div class="col-md-4 col-sm-4">
+                    <div class="team-member">                               
+                        <figure>
+                            <img src="/storage/teo-uni.png" alt="Royal Plate - Restaurant & Catering HTML Template"/>
+
+                            <figcaption>
+                                <h4 class="member-name">Teološka fakulteta Ljubljana</h4>
+                              
+
+                                <!-- .divider start -->
+                                <div class="divider">
+                                    <span class="hr-simple"></span>
+                                </div>
+
+                              
+                            </figcaption>   
+                        </figure>
+                    </div><!-- .team-member end -->
+                </div><!-- .col-md-4 end -->
+
+                <div class="col-md-4 col-sm-4">
+                    <div class="team-member">                              
+                        <figure>
+                            <img src="/storage/glasbena-vel.jpeg" alt="Royal Plate - Restaurant & Catering HTML Template"/>
+
+                            <figcaption>
+                                <h4 class="member-name"> Glasbena šola Velenje</h4>
+                              
+
+                                <!-- .divider start -->
+                                <div class="divider">
+                                    <span class="hr-simple"></span>
+                                </div>
+
+                                
+                            </figcaption>    
+                        </figure>
+                    </div><!-- .team-member end -->
+                </div><!-- .col-md-4 end -->
+            </div><!-- .row end -->
+        </div><!-- .container end -->
+    </div><!-- .page-content end -->
+
+
+
+    @endsection
--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -2,4 +2,155 @@
 
 @section('content')
  
-@endsection
+<!-- .page-content start -->
+<div class="page-content custom-img-background dark page-title page-title-1 mb-100" style="background-image: url('/storage/catering123.jpeg');">
+        <div class="container">
+            <!-- .row start -->
+            <div class="row">
+                <!-- .col-md-12 start -->
+                <div class="col-md-12 centered">
+                    <div class="custom-heading style-1 triggerAnimation animated" data-animate='fadeInUp'>
+                        <h1><span>Specializirana trgovinica za malice</span></h1>
+                        <h1>MAŠ za MAL´CO</h1>
+                    </div><!-- .custom-heading.style-1 end -->
+                </div><!-- .col-md-12 end -->
+            </div><!-- .row end -->
+        </div><!-- .container end -->
+    </div><!-- .page-content end -->
+
+    <!-- .page-content start -->
+    <div class="page-content">
+        <div class="container">
+            <p> Na Tomšičevi ulici 10b, v Velenju smo za vas odprli specializirano trgovinico za malice. Poimenovali smo jo MAŠ za MAL´CO.
+
+                    Trgovina je odprta vsak dan od 7:00 do 14:00 ure. Počasi dobiva svojo pravo podobo.
+                    
+                    V njej dobite vse, od odlične kavice Barcaffe TO GO pa vse do sestavljene malice.</p> <br>
+            <!-- .row start -->
+            <div class="row">
+                <!-- .col-md-6 start -->
+                <div class="col-md-6 col-sm-6">
+                    <img src="/storage/mas-zamalco1.jpg" alt="Royal Plate - Restaurant & Catering HTML Template" />
+                </div><!-- .col-md-6 end -->
+                <!-- .col-md-6 start -->
+                <div class="col-md-6 col-sm-6 centered">
+                    <div class="custom-heading style-1">
+                        
+                        <h3>ZAKAJ V MAŠ za MAL´CO:</h3>
+
+                        <!-- .divider.style-1 start -->
+                        <div class="divider style-1 center">
+                            <span class="hr-simple left"></span>
+                            <i class="fa fa-circle hr-icon"></i>
+                            <span class="hr-simple right"></span>
+                        </div>
+                    </div><!-- .custom-heading.style-1 end -->
+
+
+                   <ul style="  text-align: left;">
+                       <li>- širok izbor sendvičev, tortilj, solat  “NAREDI SI SAM”
+                       </li>
+                       <li>- rogljički, pecivo, bureki
+                        </li>
+                        <li>- pijača, kavica, jogurti
+                            </li>
+                            <li>- širok izbor konkretnih jedi, juhe, enolončnice
+                                </li>
+                                <li>- prijeten ambinet z brezplačnim omrežjem (free WI-FI)
+                                    </li>
+                   </ul>
+
+                </div><!-- .col-md-6 end -->
+            </div><!-- .row end -->
+        </div><!-- .container end -->
+    </div><!-- .page-content end -->
+
+    <!-- .page-content start -->
+    <div class="page-content">
+        <div class="container">
+            <!-- .row start -->
+            <div class="row">
+                <!-- .col-md-6 start -->
+                <div class="col-md-6 col-sm-6  centered">
+                    <div class="custom-heading style-1">
+                       
+                        <h3>IZVRSTNA PONUDBA</h3>
+
+                        <!-- .divider.style-1 start -->
+                        <div class="divider style-1 center">
+                            <span class="hr-simple left"></span>
+                            <i class="fa fa-circle hr-icon"></i>
+                            <span class="hr-simple right"></span>
+                        </div>
+                    </div><!-- .custom-heading.style-1 end -->
+
+                    <p>Glede na povpraševanje strank, bomo ponudbo seveda tudi širili. Seveda pa ne smemo pozabiti, da za obiskovalce pogosto poskrbimo s kakšnim presenečenjem: popusti, gratis kavico, darilo ob nakupu,…
+
+                            Vabljeni ste, da nas obiščete in se prepričate o naši dobri ponudbi.</p>
+                </div><!-- .col-md-6 end -->
+                <!-- .col-md-6 start -->
+                <div class="col-md-6 col-sm-6">
+                    <img src="/storage/mas-zamalco2.jpg" alt="Royal Plate - Restaurant & Catering HTML Template" />
+                </div><!-- .col-md-6 end -->
+            </div><!-- .row end -->
+        </div><!-- .container end -->
+    </div><!-- .page-content end -->
+
+    <!-- .page-content start -->
+    <div class="page-content">
+        <div class="container">
+            <!-- .row start -->
+            <div class="row mb-0">
+                <!-- .col-md-12 start -->
+                <div class="col-md-12 centered mb-0">
+                    <div class="custom-heading style-1">
+                        <h2><span>Obiščite nas</span></h2>
+                        <h2>NAŠA LOKACIJA</h2>
+
+                        <!-- .divider.style-2 start -->
+                        <div class="divider style-2 center">
+                            <span class="hr-double left"></span>
+                          
+                            <span class="hr-double right"></span>
+                        </div>
+                    </div><!-- .custom-heading.style-1 end -->
+                </div><!-- .col-md-12 end -->
+            </div><!-- .row end -->
+            <!-- .row start -->
+            <div class="row">
+                <!-- .col-md-4 start -->
+                <div class="col-lg-12 col-md-12 col-sm-12 ">
+                    <div class="custom-heading style-2">
+                        <h4>MAŠ ZA MAL´CO</h4>
+                        <div class="divider style-1">
+                            <i class="fa fa-circle hr-icon left"></i>
+                            <span class="hr-simple right"></span>
+                        </div>
+                    </div><!-- .custom-heading.style-2 end -->
+
+                    <ul>
+                        <li>Tomšičeva ulica 10b,</li>
+                        <li>3320 Velenje</li>
+                        <li>Slovenija</li>
+                    </ul>
+                    <h5>ODPIRALNI ČAS:</h5>
+                    <ul>
+                        <li>Pon - Pet:  7:00 do 14:00</li>
+                    </ul>
+                    <h5>KONTAKT:</h5>
+                    <ul>
+                        <li>Telefon: +386 40 851 282
+                               </li>
+                        <li> Email: info@mas-catering.si</li>
+                    </ul>
+                    
+                </div><!-- .col-md-4 end -->
+
+                
+            </div><!-- .row end -->
+        </div><!-- .container end -->
+    </div><!-- .page-content end -->
+
+
+
+    @endsection
