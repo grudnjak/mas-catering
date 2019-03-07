a355b4aa8806e0fbc7def914f4784633a721115b
Valentin Grudnik <wallicar99@gmail.com>
2019-03-07 17:38:13 +0100

    Theme install

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,38 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Royal Plate is a HTML template created for restaurants and catering companies.">
+    <meta name="author" content="pixel-industry">
+    <meta name="keywords" content="CSS, HTML5, clean, restaurant, jQuery, retina, bootstrap">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/bootstrap.min.css"/><!-- bootstrap grid -->
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css"/><!-- bootstrap theme -->
+    <link rel="stylesheet" href="css/style.css"/><!-- template styles -->
+    <link rel="stylesheet" href="css/color-default.css"/><!-- default template color styles -->
+    <link rel="stylesheet" href="css/retina.css"/><!-- retina ready styles -->
+    <link rel="stylesheet" href="css/responsive.css"/><!-- responsive styles -->
+    <link rel="stylesheet" href="css/animate.css"/><!-- animation for content -->
+    <link rel="stylesheet" href="masterslider/style/masterslider.css" /><!-- Master slider css -->
+    <link rel="stylesheet" href="masterslider/skins/default/style.css" /><!-- Master slider default skin -->
+    <link rel='stylesheet' href='owl-carousel/owl.carousel.css'/><!-- Events carousel -->
+    <link rel='stylesheet' href='owl-carousel/owl.theme.css'/><!-- Events carousel -->
+
+    <!-- Magnific Popup - image lightbox -->
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+
+    <!-- Google Web fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Suranna' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Montez' rel='stylesheet' type='text/css'>
+
+    <!-- Font icons -->
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css"/><!-- Font awesome icons -->
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
@@ -16,65 +40,33 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!--
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> Styles -->
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+            @include('inc.navbar')
+        
 
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+
+    <script src="js/jquery-2.1.4.min.js"></script><!-- jQuery library -->
+<script src="js/bootstrap.min.js"></script><!-- .bootstrap script -->
+<script src="js/jquery.scripts.min.js"></script><!-- modernizr, retina, stellar for parallax -->  
+<script src="masterslider/masterslider.min.js"></script><!-- Master slider main js -->
+<script src="masterslider/jquery.easing.min.js"></script><!-- Master slider easing js -->
+<script src="js/jquery.magnific-popup.min.js"></script><!-- used for image lightbox -->
+<script src="owl-carousel/owl.carousel.min.js"></script><!-- Carousels script -->
+<script src="js/jquery.dlmenu.min.js"></script><!-- for responsive menu -->
+<script src="js/include.js"></script><!-- custom js functions -->
+<script src="js/instagram-stream.jquery.js"></script><!-- instagram stream plugin -->
+<script src="js/TweenMax.min.js"></script> <!-- Plugin for smooth scrolling-->
+<script src="js/retina.min.js"></script><!-- retina images script -->
+<script src="js/ScrollToPlugin.min.js"></script> <!-- Plugin for smooth scrolling-->
 </body>
+
+
 </html>
